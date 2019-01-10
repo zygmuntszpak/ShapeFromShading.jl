fd460cdf914864a6ae1ea98f1d80cb4f22a851db
Tristan <betttris13@yahoo.com.au>
2019-01-10 13:12:42 +1030

    implementation of estimateAlbedo

--- src/estimatealbedo.jl
+++ src/estimatealbedo.jl
@@ -1,42 +1,15 @@
-function gradient(array::Array{T,2} where T <: Real)
-    #calculate spatial gradient of array
-    δx = similar(array)
-    δy = similar(array)
-    for i = 1:size(array,1)
-        for j = 1:size(array,2)
-            if i == 1
-                δx[i,j] = array[i+1,j] - array[i,j]
-            elseif i == size(array,1)
-                δx[i,j] = array[i,j] - array[i-1,j]
-            else
-                δx[i,j] = (array[i+1,j] - array[i-1,j])/2
-            end
-            if j == 1
-                δy[i,j] = array[i,j+1] - array[i,j]
-            elseif j == size(array,2)
-                δy[i,j] = array[i,j] - array[i,j-1]
-            else
-                δy[i,j] = (array[i,j+1] - array[i,j-1])/2
-            end
-        end
-    end
-    return δx, δy
-end
-
 function estimate_img_properties(img::AbstractArray)
     E = Array{Float64}(img)
+
     #calculate spatial gradient of img
-    Ex,Ey = gradient(E)
+    Ey,Ex = imgradients(E, KernelFactors.scharr, "replicate")
+
     #normalize gradient
     nEx = similar(E)
     nEy = similar(E)
-    Exy,~ = gradient(Ey)
-    for i = 1:size(E,1)
-        for j = 1:size(E,2)
-            nEx[i,j] = Ex[i,j]/(Exy[i,j]+1^(-50))
-            nEy[i,j] = Ey[i,j]/(Exy[i,j]+1^(-50))
-        end
-    end
+    Exy = sqrt.((Ex.^2) .+ (Ey.^2))
+    nEx = Ex./(Exy.+(2^(-52)))
+    nEy = Ey./(Exy.+(2^(-52)))
 
     #calculate means
     μ₁ = mean(E)
