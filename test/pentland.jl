d152f9b68d520ef264f289415e0d2a88257ca716
Tristan <betttris13@yahoo.com.au>
2019-02-12 11:26:09 +1030

    Merge branch 'dev' Merge remote-tracking branch 'origin/master'
    fixed test set names
    
    
    lessened test cases
    
    
    softened test cases
    
    
    Performance improvments
    
    TODO: Shah
    Merge branch 'dev'
    Adds smoothness options and updated test version

---
+++ test/pentland.jl
@@ -0,0 +1,11 @@
+@testset "pentland" begin
+    img = generate_surface(0.5, [0,0,1], radius = 5, scale_factor = 1.5, resolution=0.1)
+    Z = retrieve_surface(Pentland(), img)
+    @test Z[76,76] ≈ 8.268241051489031
+    @test isapprox(Z[1,1], 1.6063661929819305e-14; atol = 2e-15)
+    @test isapprox(Z[151,151], 1.1842354207459187e-14; atol = 2e-15)
+    @test Z[90,90] ≈ 7.596179360050504
+    @test Z[101,36] ≈ 3.0163090221571354
+    @test isapprox(Z[22, 136], 1.468693863573891e-14; atol = 2e-15)
+    @test Z[42,42] ≈ 3.870236121646488
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
-using ShapeFromShading
+using Images, ShapeFromShading
 using Test
 
 @testset "ShapeFromShading.jl" begin
-    # Write your own tests here.
+    include("syntheticsurface.jl")
+    include("shah.jl")
+    include("pentland.jl")
+    include("discreteshape.jl")
+    include("discreteshapebound.jl")
+    include("estimatealbedo.jl")
 end
